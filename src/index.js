17b842a3afadd0fe5899d046b91230eb9b730bb8
ShogunTokugawa <xaxol581994@gmail.com>
2020-06-10 01:06:07 +0400

    lesson easy 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {
-    z11
-} from "./lesons/easy/task1";
-let stroka = "У лукоморья дуб зелёный. Златая цепь на дубе том. И днём и ночью кот учёный. Всё ходит по цепи кругом";
-console.log(z11(stroka));
+    z2
+} from "./lesons/easy/task2";
+let name = "Зина";
+console.log(z2(name));
