8167873ebd3410aa52e85c47353268a6778fdd69
ShogunTokugawa <xaxol581994@gmail.com>
2020-06-10 00:10:26 +0400

    lesson easy 1

---
+++ src/lesons/easy/task1.js
@@ -0,0 +1,10 @@
+
+export function z11(stroka) {
+    let arr = stroka.split('.');
+    for (let item of arr) {
+        console.log(itemTrim.slice(0, itemTrim.indexOf(' ')));
+    }
+    return arr;
+}
+
+//export const z12 = arr => arr.every(item => item % 2 === 0);
